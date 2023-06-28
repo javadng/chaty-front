c3f66829edf861364c2a1fca0b7d32b792a7ed96
javadng <javadnasirifu@gmail.com>
2023-06-28 22:41:39 +0330

    master: auth response show

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // export const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:3001';
-export const SOCKET_URL =  'http://localhost:3001';
-// export const SOCKET_URL = 'https://chat-backend.iran.liara.run';
+// export const SOCKET_URL =  'http://localhost:3001';
+export const SOCKET_URL = 'https://chat-backend.iran.liara.run';
 // https://chat-backend.iran.liara.run
