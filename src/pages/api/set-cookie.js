b053a39e814a390df3e8d5770e6f0e2ffedc123f
javadng <javadnasirifu@gmail.com>
2023-06-29 17:46:08 +0330

    master: test

--- src/pages/api/set-cookie.js
+++ src/pages/api/set-cookie.js
@@ -2,7 +2,7 @@ const Cookies = require('cookies');
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(400).json({ message: 'Bad request' });
+    return res.status(500).json({ message: 'Bad request' });
   }
 
   const cookies = new Cookies(req, res);
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     httpOnly: true,
   };
 
-  if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
+  // if (process.env.NODE_ENV === 'production') cookieOption.secure = true;
   cookies.set('jwt', req.body.token, cookieOption);
 
   res.status(200).json({ message: 'successfully login and cookies set.' });
