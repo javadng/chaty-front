e89551e62acdafa778454df9805dea85c13808ed
javadng <javadnasirifu@gmail.com>
2023-06-29 18:05:12 +0330

    master: change ui

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -16,24 +16,28 @@ const AuthPage = props => {
   const btnclass = 'py-3 rounded-3xl transition duration-500';
 
   return (
-    <div className="auth container mx-auto max-w-md bg-blue-c h-full m-5 mt-10 py-5 rounded-xl">
-      <h1 className="text-white text-4xl mb-8 text-center">Chaty app</h1>
-      <div className="bg-gray-transparent text-gray-light p-1.5 w-4/5 grid grid-cols-2 mx-auto rounded-3xl">
-        <button
-          onClick={toggleLoginState}
-          className={`${btnclass} ${isLoggin ? 'bg-white text-blue-c ' : ''}`}
-        >
-          Login
-        </button>
-        <button
-          onClick={toggleLoginState}
-          className={`${btnclass} ${!isLoggin ? 'bg-white text-blue-c ' : ''}`}
-        >
-          SignUp
-        </button>
+    <div className="px-3">
+      <div className="auth container mx-auto max-w-md bg-blue-c h-full my-10 mb-32 py-5 rounded-xl">
+        <h1 className="text-white text-4xl mb-8 text-center">Chaty app</h1>
+        <div className="bg-gray-transparent text-gray-light p-1.5 w-4/5 grid grid-cols-2 mx-auto rounded-3xl">
+          <button
+            onClick={toggleLoginState}
+            className={`${btnclass} ${isLoggin ? 'bg-white text-blue-c ' : ''}`}
+          >
+            Login
+          </button>
+          <button
+            onClick={toggleLoginState}
+            className={`${btnclass} ${
+              !isLoggin ? 'bg-white text-blue-c ' : ''
+            }`}
+          >
+            SignUp
+          </button>
+        </div>
+        {isLoggin && <SignIn />}
+        {!isLoggin && <SignUp />}
       </div>
-      {isLoggin && <SignIn />}
-      {!isLoggin && <SignUp />}
     </div>
   );
 };
